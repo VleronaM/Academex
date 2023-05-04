d00915cfdfa619a017324fe93e75b6a44b0b1845
Vlerona <vm58482@ubt-uni.net>
2023-05-04 23:14:56 +0200

    Header and Footer

---
+++ src/components/Common/Common/Footer.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './style/footer.css';
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  
+    return(
+        <>
+        <section className = "newletter">
+          <div className="container flexSB">
+            <div className="left row">
+             <h1>Newsletter- Stay tune and get the latest update</h1>
+             <span>far far away, behind the word mountains</span>
+            </div>
+            <div className="right row">
+                <input type= 'text' placeholder="Enter email address"/>
+             <i className= 'fa fa-paper-plane'></i>
+            </div>
+          </div>
+        </section>
+        <footer>
+            <div className="container padding">
+                <div className= "box logo">
+                    <h1>ACADEMEX</h1>
+                    <span>E-Learning</span>
+                    <p>asld;fkagengbklwfnsbkw;lskefvs;ldxgkbrs;lfhnbwaikewlzrnfiwrhnipetdjhpowkatinq4hjwrksfhnbwistgpoq4eagqearqo;argwruhiwosezjrfmq;etgw;t10prof urime dhe pergezime</p>
+                    <i className='fab fa-facebook-f icon'></i>
+                    <i className='fab fa-instagram-f icon'></i>
+                    <i className='fab fa-youtube icon'></i>
+                    <i className='fab fa-twitter icon'></i>
+                </div>
+                <div className="box link">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/Common/Common/Header.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Head from "./Head";
+import "./style/header.css"
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const[click, setClick] = useState(false)
+  return(
+    <div>
+      <Head/>
+      <header>
+        <nav className='flexSB'>
+        <ul className = {click ? "mobile-nav" : "flexSB"} onClick={() => setClick(false)}>
+          <li>
+            <Link to='/'>Home</Link>
+            </li>
+          <li>
+            <Link to='/courses'>Courses</Link>
+            </li>
+          <li>
+            <Link to='/books'>Books</Link>
+            </li>
+          <li>
+            <Link to='/About'>About</Link>
+            </li>
+          <li>
+            <Link to='/team'>Team</Link>
+            </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+            </li>
+        </ul>
+        <div className="start">
+        <div className = "button"> Get Certificate</div>
+        </div>
+        <button className= 'toggle'  onClick={() => setClick(!click)}>
+    {click ? <i className='fa fa-times' ></i> : <i className= 'fa fa-bars'></i>}
+        </button>
+        </nav>
+      </header>
+    </div>
+  )
+}
+export default Header;
+
+/*import './style/header.css';
+const Header = () => {
+  
+     return (
+        <div>
+          <section className = 'head'>
+        <div className='container'>
+          <div className = 'logo'>
+        <h1>ACADEMEX</h1>  
+        <span>E-Learning</span>
+           </div>
+     <div className = 'social'>
+          <i className= 'fab fa-facebook-f'></i>
+          <i className= 'fab fa-instagram icon'></i>
+          <i className= 'fab fa-youtube icon'></i>
+          <i className= 'fab fa-twitter icon'></i>
+     </div>
+
+        </div>
+        </section> 
+        </div>
+     )
+  
+}*/
