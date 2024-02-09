f073a112d8800ba878875df59a614ac6c1033696
Eljesa Krasniqi <ek61368@ubt-uni.net>
2024-02-09 13:55:29 +0100

    connected books to backend

--- src/components/books/BooksCard.jsx
+++ src/components/books/BooksCard.jsx
@@ -1,32 +1,45 @@
-import Title from '../Common/title/Title'
-import React from 'react'
-import { books } from '../../database'
-import "./book.css"
+import React, { useState, useEffect } from 'react';
+import Title from '../Common/title/Title';
+import './book.css';
 
 const BooksCard = () => {
-    return (
-        < >
-            <section className='books'>
-                <div className='container'>
-                    <Title title='Download Your Favorite Books for Free!' />
-                    <div className='content grid3'>
-                        {books.map((val) => (
-                            <div className="box">
-                                <div className="img">
-                                    <img src={val.cover} alt="" />
-                                </div>
-                                <h1>{val.name}</h1>
-                                <span>{val.author}</span>
-                                <div>
-                                    <button className='downloads'>Download now<i class="fa fa-download" aria-hidden="true"></i></button>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+  const [books, setBooks] = useState([]);
 
-export default BooksCard
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('http://localhost:3030/books')
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []); // Empty dependency array to run the effect once on mount
+
+  return (
+    <>
+      <section className='books'>
+        <div className='container'>
+          <Title title='Download Your Favorite Books for Free!' />
+          <div className='content grid3'>
+            {books.map((val) => (
+              <div className="box" key={val.id}>
+                <div className="img">
+                  <img src={val.image} alt="" />
+                </div>
+                <h1>{val.title}</h1>
+                <span>{val.author}</span>
+                <div>
+                  <button className='downloads'>
+                    <a href={val.link} className='button-link' target="_blank" rel="noopener noreferrer">
+                      Read now<i className="fa fa-download" aria-hidden="true"></i>
+                    </a>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BooksCard;
